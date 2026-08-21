88ff85307c6e8ff5ee5b8e864dfe5a6af0c6ce04
navvenkumarpvt-sketch <navvenkumarpvt@gmail.com>
2026-08-21 21:43:19 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> set = new HashSet<>();
+
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                int currChar = board[i][j];
+
+                if(currChar!='.'){
+
+                    if(!set.add(currChar + "in row " + i) || !set.add(currChar+"in column "+ j)|| !set.add(currChar + "in box " + i/3 + '-' + j/3)){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
