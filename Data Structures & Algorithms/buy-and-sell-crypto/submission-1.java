75f0fd5834efb5bcb5203fde0821d71ae04cb329
navvenkumarpvt-sketch <navvenkumarpvt@gmail.com>
2026-08-22 21:13:11 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int lowest = prices[0];
+        int maxProfit = 0;
+        for(int i=1;i<prices.length;i++){
+            lowest = Math.min(lowest, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i]-lowest);
+        }
+        return maxProfit;
+    }
+}
